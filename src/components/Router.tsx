66c8e7c5f22753069d1c1211c098af69f24c659f
Елизавета Филиппова <lisa-philippova@yandex.ru>
2021-05-03 23:21:35 +0300

    8 - merge conflicts fix

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,78 +2,78 @@ import React from 'react';
 import {
     BrowserRouter,
     Switch,
-    Route
+    Route,
+    Redirect,
 } from 'react-router-dom';
-import {Home} from "../pages/Home";
-import {Profile} from "../pages/Profile";
-import {Payments} from "../pages/Payments";
-import {Grades} from "../pages/Grades";
-import {Messages} from "../pages/Messages";
-import {Settings} from "../pages/Settings";
-import {NavBar} from "./NavBar";
-import {Courses} from "../pages/Courses";
+import {Container} from 'react-bootstrap';
+import {Home} from '../pages/Home';
+import {Profile} from '../pages/Profile';
+import {Payments} from '../pages/Payments';
+import {Courses} from '../pages/Courses';
+import {Grades} from '../pages/Grades';
+import {Messages} from '../pages/Messages';
+import {Settings} from '../pages/Settings';
+import {Navigation} from '../components/Navigation';
 
-export interface RouterProps {
-    main: string;
-}
+const routes = [
+    {
+        label: 'Главная',
+        path: '/',
+        component: Home,
+    },
+    {
+        label: 'Профиль',
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        label: 'Оплаты',
+        path: '/payments',
+        component: Payments,
+    },
+    {
+        label: 'Занятия',
+        path: '/courses',
+        component: Courses,
+    },
+    {
+        label: 'Аттестации',
+        path: '/grades',
+        component: Grades,
+    },
+    {
+        label: 'Сообщения',
+        path: '/messages',
+        component: Messages,
+    },
+    {
+        label: 'Настройки',
+        path: '/settings',
+        component: Settings,
+    },
+];
 
-export interface TabDescriptor {
-    caption: string;
-    path: string;
-}
-
-function createTab(caption: string, path: string): TabDescriptor {
-    return {caption, path};
-}
-
-export const tabs = [
-    createTab('Главная', '/'),
-    createTab('Профиль', '/profiles'),
-    createTab('Оплаты', '/payments'),
-    createTab('Занятия', '/courses'),
-    createTab('Аттестации', '/grades'),
-    createTab('Сообщения', '/messages'),
-    createTab('Настройки', '/settings')];
-
-export class Router extends React.Component<RouterProps> {
-
-    constructor(props: RouterProps) {
-        super(props);
-    }
-
-    public render() {
-        return (
-            <BrowserRouter>
-                <NavBar tabs={tabs} active={this.props.main}/>
-                <div>
-                    <Switch>
-                        <Route exact path={this.getPath('Главная')}>
-                            <Home/>
-                        </Route>
-                        <Route path={this.getPath('Профиль')}>
-                            <Profile/>
-                        </Route>
-                        <Route path={this.getPath('Оплаты')}>
-                            <Payments/>
-                        </Route>
-                        <Route path={this.getPath('Занятия')}>
-                            <Courses/>
-                        </Route>
-                        <Route path={this.getPath('Аттестации')}>
-                            <Grades/>
-                        </Route>
-                        <Route path={this.getPath('Сообщения')}>
-                            <Messages/>
-                        </Route>
-                        <Route path={this.getPath('Настройки')}>
-                            <Settings/>
-                        </Route>
-                    </Switch>
-                </div>
-            </BrowserRouter>)
-    }
-
-    private getPath(caption: string): string {
-        return tabs.find(x => x.caption === caption)?.path || "/";
-    }
-}
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Navigation
+                navigationLinks={routes.map((route) => {
+                    return {label: route.label, path: route.path};
+                })}
+            />
+            <Container>
+                <Switch>
+                    {routes.map((route, i) => (
+                        <Route
+                            exact
+                            key={i}
+                            path={route.path}
+                            render={(props) => <route.component {...props} />}
+                        />
+                    ))}
+                    <Redirect to={'/'}/>
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    );
+};
